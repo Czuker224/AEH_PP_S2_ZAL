d93a1c82ab9bb76f0bd9890d9b4f25e7d4bd9ffc
Czuker224 <56686494+Czuker224@users.noreply.github.com>
2024-05-08 22:22:05 +0200

    dodanie bazy danych do projektu. Dodanie klasy odpowiedzialnej za połączenie z bazą danych

---
+++ src/main/java/db/dbConnection.java
@@ -0,0 +1,25 @@
+package db;
+
+import java.util.logging.Logger;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class dbConnection {
+    private static final String DATABASE_URL = "src/main/resources/db/database";
+    private static final Logger logger = Logger.getLogger(dbConnection.class.getName());
+
+    public static Connection createDatabaseConnection() {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection("jdbc:sqlite:" + DATABASE_URL);
+            logger.info("Database connection established");
+        } catch (SQLException e) {
+            logger.severe("Database connection failed: " + e);
+        }
+        if (connection == null) {
+            logger.severe("Database connection = null");
+        }
+        return connection;
+    }
+}
