c384f7f17edbb8e606a1b8de132f2a45d464fa54
Czuker224 <56686494+Czuker224@users.noreply.github.com>
2024-05-19 10:29:17 +0200

    Usunięcie niepotrzebnego kodu z klasy App
    Usunięto nieużywane importy i niepotrzebny kod z klasy App. Teraz klasa jest znacznie bardziej odpowiednia i łatwiejsza do czytania. Przede wszystkim usunięto kody odpowiedzialne za obsługę notatek, które były nieużywane.

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,12 +1,6 @@
-import notes.AddNote;
-import notes.Note;
+
 import session.Session;
-import logowanie.LogIn;
 import user.User;
-
-import java.util.Scanner;
-import java.util.Date;
-
 
 public class App {
 
@@ -22,29 +16,5 @@ public class App {
 
         }
 
-        // Utworzenie sesji dla zalogowanych użytkowników
-        if(currentUser == null){
-            return;
-        }
-
-//        AddNote note = new AddNote(1,"New","Ważne-Pilne","testowa notatka sprawdzająca",new Date());
-//        note.save();
-//        note.printNote();
-
-
-//        Note note = new Note(1);
-//        if(note.getId() != null){
-//            note.printNote();
-//        }else{
-//            System.out.println("Brak danych do wyświetlenia");
-//        }
-
-
-
-
-        //Zakończenie sesji
-        if(currentSession != null){
-            currentSession.endSession();
-        }
     }
 }
